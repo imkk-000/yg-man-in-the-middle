f4d9be10961dec057f39ff25da1cd2be985e6e57
Nattakit Boonyang <nattakit.boonyang@gmail.com>
2019-08-18 23:36:12 +0700

    Add: config model

---
+++ model/config.go
@@ -0,0 +1,28 @@
+package model
+
+import (
+	"encoding/json"
+	"io/ioutil"
+)
+
+type IpConfig struct {
+	IP   string `json:"ip"`
+	Port int    `json:"port"`
+}
+
+type Config struct {
+	Server IpConfig `json:"server"`
+	Login  IpConfig `json:"login"`
+}
+
+func (Config) Read(filename string) (config Config) {
+	data, err := ioutil.ReadFile(filename)
+	if err != nil {
+		panic(err)
+	}
+	err = json.Unmarshal(data, &config)
+	if err != nil {
+		panic(err)
+	}
+	return
+}
